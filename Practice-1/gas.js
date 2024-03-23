8a3b0c79d8baf15ec4daf2021673bcb2a125a303
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-23 20:17:58 +0530

    Update gas.js

---
+++ Practice-1/gas.js
@@ -0,0 +1,35 @@
+console.log("I love to code everytime");
+var firstName = "Ankur";
+console.log(firstName);
+// changing the value of the variable
+firstName = "Rohit";
+console.log(firstName);
+
+//Variable name cannot start with number
+//Variable name can only start with underscore & dollar symbol
+var first_name = "Mohit"; // snake case writing
+var firstName = "Rohit"; // camel case writing
+console.log(firstName, first_name);
+
+let name1 = "Yash"; //let keyword
+name1 = "Arsh";
+console.log(name1);
+
+const pi = 3.14; //declaring constant value we use const
+console.log(pi);
+
+let string = "coding is my passion";
+console.log(string[0]); //tells value from its index
+console.log(string.length); //lenght of the string
+console.log(string[string.length - 2]); //last index = lenght - 1
+
+let name2 = "Aman";
+console.log(name2);
+name2 = name2.trim();
+console.log(name2);
+name2 = name2.toUpperCase();
+console.log(name2);
+name2 = name2.toLowerCase();
+console.log(name2);
+name2 = name2.slice(2);
+console.log(name2);
