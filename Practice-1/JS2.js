a7bc44ec23918657cacc162f9b2ef4e0ed3e0949
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-23 21:53:28 +0530

    Update JS2.js

--- Practice-1/JS2.js
+++ Practice-1/JS2.js
@@ -11,3 +11,21 @@ console.log(typeof string); //string --> number by using + or -
 let marks = "95";
 marks = Number(marks); //string --> number
 console.log(typeof marks);
+
+let num1 = 34;
+let num2 = 67;
+let num_string1 = num1 + num2;
+console.log(num_string1);
+
+let num3 = "34";
+let num4 = "67";
+let num_string2 = +num3 + +num4;
+console.log(num_string2);
+
+let age1 = 19;
+let name = "Ankur";
+let about = "Hello I am " + name + " and I am " + 19 + " years old "; 
+console.log(about);
+let about2 = `Hello I am ${name} and my age is ${age}`;
+console.log(about2);
+
