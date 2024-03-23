ffa813d59e6566e58bacc026f712af98171d56f7
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-23 22:40:10 +0530

    Update JS3.js

--- Practice-1/JS3.js
+++ Practice-1/JS3.js
@@ -1,1 +1,13 @@
+let num1 = BigInt(12); //BitInt
+let num2 = 123n;
+console.log(num1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(num1 + num2);
+
+let num1 = 50;
+let num2 = "50";
+console.log(num1 < num2);
+console.log(num1 === num2);
+console.log(num1 == num2);
+console.log(num1 !== num2);
 
