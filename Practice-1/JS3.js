df542d6df91075bceb19ff7b98a631e719aa2630
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-23 22:46:41 +0530

    Update JS3.js

--- Practice-1/JS3.js
+++ Practice-1/JS3.js
@@ -4,6 +4,7 @@ console.log(num1);
 console.log(Number.MAX_SAFE_INTEGER);
 console.log(num1 + num2);
 
+//boolean values
 let num1 = 50;
 let num2 = "50";
 console.log(num1 < num2);
@@ -11,3 +12,10 @@ console.log(num1 === num2);
 console.log(num1 == num2);
 console.log(num1 !== num2);
 
+//if else condition
+let value = 45;
+if (value > 40) {
+  console.log("bigger value");
+} else if (value < 40) {
+  console.log("smaller value");
+}
