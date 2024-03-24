5a5397ed9c5c3e9a5afd113ec2057be38b5e48e9
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-24 11:54:40 +0530

    Update JS3.js

--- Practice/JS3.js
+++ Practice/JS3.js
@@ -20,3 +20,60 @@ if (value > 40) {
 } else if (value < 40) {
   console.log("smaller value");
 }
+
+let age1 = 19;
+let name = "Ankur";
+let about = "Hello I am " + name + " and I am " + 19 + " years old ";
+console.log(about);
+let about2 = `Hello I am ${name} and my age is ${age}`;
+console.log(about2);
+
+let value = 45;
+if (value > 40) {
+  console.log("bigger value");
+} else if (value < 40) {
+  console.log("smaller value");
+}
+
+let num = 13;
+if (num % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+let value1 = 0;
+
+if (value1) {
+  console.log(firstName);
+} else {
+  console.log("firstName is kinda empty");
+}
+
+let num3 = "34";
+let num4 = "67";
+let num_string2 = +num3 + +num4;
+console.log(num_string2);
+
+let num = 13;
+if (num % 2 === 0) {
+  console.log("even");
+} else {
+  console.log("odd");
+}
+
+let value1 = 0;
+
+if (value1) {
+  console.log(firstName);
+} else {
+  console.log("firstName is kinda empty");
+}
+
+let age3 = 17;
+let drink1 = age3 >= 18 ? "drink" : "milk";
+console.log(drink1);
+
+
+
+
