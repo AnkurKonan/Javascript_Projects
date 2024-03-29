0323ff10ae2a82f8890d6f3d4b3d103fb569b44c
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-29 13:55:17 +0530

    Create JS4.js

---
+++ Practice/JS4.js
@@ -0,0 +1,26 @@
+let winningnumber = 14;
+let userguess = 56;
+if (winningnumber === userguess) {
+  console.log("Congratulation!!! and Celebrations!!!");
+} else {
+  if (winningnumber > userguess) {
+    console.log("Too low!!!");
+  } else {
+    console.log("Too high!!!");
+  }
+}
+
+let tempInDegree = 50;
+if (tempInDegree > 40) {
+  console.log("too hot");
+} else if (tempInDegree > 30) {
+  console.log("lets go for swim");
+} else if (tempInDegree > 20) {
+  console.log("weather is cool");
+} else if (tempInDegree > 10) {
+  console.log("it is very cold outside");
+} else {
+  console.log("extremely cold");
+}
+console.log("hello");
+
