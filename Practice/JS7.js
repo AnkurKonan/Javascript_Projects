8910440558ebb5f25ee9e020cd61a052a9fca234
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-30 18:21:11 +0530

    Create JS7.js

---
+++ Practice/JS7.js
@@ -0,0 +1,11 @@
+let name = ["Ankur", "Ram", "vanita"];
+let name2 = [];
+for (i = 0; i < name.length; i++) {
+  name2.push(name[i].toUpperCase());
+}
+console.log(name2);
+
+let fruits = ["apple", "mango", "grapes", "banana"];
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
