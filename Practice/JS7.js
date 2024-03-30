bd4282918a92c2348163800f3e72a7ddb6364e54
Ankur Konan <112815485+AnkurKonan@users.noreply.github.com>
2024-03-30 19:35:50 +0530

    Update JS7.js

--- Practice/JS7.js
+++ Practice/JS7.js
@@ -9,3 +9,24 @@ let fruits = ["apple", "mango", "grapes", "banana"];
 for (let i = 0; i <= 9; i++) {
   console.log(i);
 }
+
+const name3 = {
+  name: "Ram",
+  age: "34",
+  salary: "300000",
+  skills: ["Marketing", "Gas", "content creating"],
+};
+console.log(name3);
+console.log(name3["name"]);
+console.log(name3["age"]);
+console.log(name3.skills);
+
+let name3 = ["Ankur", "Ram", "vanita"];
+let name4 = [];
+let i = 0;
+while (i < name.length) {
+  name4.push(name[0].toUpperCase());
+  i++;
+}
+console.log(name4);
+
